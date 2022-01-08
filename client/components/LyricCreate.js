d6d7104c212f5257dc58be88c3e5fd54c7a9b3f0
Michael Maliniak <mike@maliniak.io>
2022-01-08 12:25:54 +1000

    updated mongoose methods

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LyricCreate extends React.Component {
+	render() {
+		return (
+			<form>
+				<label>Add a Lyric</label>
+				<input type="text" />
+			</form>
+		);
+	}
+}
+
+export default LyricCreate;
--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import fetchSong from "../queries/fetchSong";
 import { Link } from "react-router";
+import LyricCreate from "./LyricCreate";
 
 class SongDetail extends Component {
 	constructor(props) {
@@ -15,6 +16,7 @@ class SongDetail extends Component {
 			<div className="container">
 				<Link to="/">Back</Link>
 				<h3>{song.title}</h3>
+				<LyricCreate />
 			</div>
 		);
 	}
